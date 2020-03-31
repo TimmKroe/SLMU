a59f20ec2ad7894bd89cb29536d8259a48e0ce54
TimmKroe <kroeger.timm@gmx.de>
2020-03-31 11:23:30 +0200

    added logging information and advanced file normalization

--- SLMU/Program.cs
+++ SLMU/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.IO.Compression;
+using Microsoft.VisualBasic;
 
 namespace SLMU
 {
@@ -14,35 +15,47 @@ namespace SLMU
 
             Console.WriteLine("\n\n");
 
+            Console.WriteLine("[INFO] Loading all Files......");
             // get all files
             DirectoryInfo d = new DirectoryInfo(@path);
             FileInfo[] files = d.GetFiles("*.jar");
+
 
             for (int i = 0; i < files.Length; i++)
             {
                 // get file name
                 FileInfo file = files[i];
-                string fileName = file.Name.ToLower().Replace("_", "-");
+                string fileName = file.Name.ToLower()
+                    .Replace("_", "-")
+                    .Replace(" ", "")
+                    .Replace("'", "")
+                    .Replace("+", "");
                 string newFileName = fileName.Replace(".jar", string.Empty);
+                Console.WriteLine("[INFO] normalizing filename - (" + file.Name + ")");
 
                 // set new locations
                 string newFolder = path + newFileName;
                 string newModsFolder = path + newFileName + "/mods";
                 string newConfigFolder = path + newFileName + "/config";
 
+                Console.WriteLine("[INFO] creating Directories for (" + newFileName + ")");
                 // create folder with name
                 Directory.CreateDirectory(newFolder);
                 Directory.CreateDirectory(newModsFolder);
                 Directory.CreateDirectory(newConfigFolder);
 
+                Console.WriteLine("[INFO] Moving file into directory");
                 // put file into mods folder inside the new folder
                 File.Move(path + file.Name, newModsFolder + "\\" + fileName);
 
-
+                Console.WriteLine("[INFO] creating zip archive...");
                 // zip that folder
                 ZipFile.CreateFromDirectory(newFolder, newFolder + ".zip");
-
+                Console.WriteLine("[INFO] (" + file.Name + ") done. \n");
             }
+
+            Console.WriteLine("[INFO] Process has finished. DONE.");
+            Console.ReadLine();
         }
     }
 }
