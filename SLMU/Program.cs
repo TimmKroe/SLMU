41554607ae372df219299bcf3dfa75cc04156c4d
TimmKroe <kroeger.timm@gmx.de>
2020-03-31 11:12:58 +0200

    refactoring
    Signed-off-by: TimmKroe <kroeger.timm@gmx.de>

--- SLMU/Program.cs
+++ SLMU/Program.cs
@@ -4,16 +4,16 @@ using System.IO.Compression;
 
 namespace SLMU
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             Console.WriteLine("Hello!");
             Console.WriteLine("Insert the Path of the Directory you want to zip the files in:");
-            var path = Console.ReadLine();
-            
+            string path = Console.ReadLine();
+
             Console.WriteLine("\n\n");
-            
+
             // get all files
             DirectoryInfo d = new DirectoryInfo(@path);
             FileInfo[] files = d.GetFiles("*.jar");
@@ -21,27 +21,27 @@ namespace SLMU
             for (int i = 0; i < files.Length; i++)
             {
                 // get file name
-                var file = files[i];
-                var fileName = file.Name.ToLower().Replace("_", "-");
-                var newFileName = fileName.Replace(".jar", string.Empty);
-                
+                FileInfo file = files[i];
+                string fileName = file.Name.ToLower().Replace("_", "-");
+                string newFileName = fileName.Replace(".jar", string.Empty);
+
                 // set new locations
-                var newFolder = path + newFileName;
-                var newModsFolder = path + newFileName + "/mods";
-                var newConfigFolder = path + newFileName + "/config";
-                
+                string newFolder = path + newFileName;
+                string newModsFolder = path + newFileName + "/mods";
+                string newConfigFolder = path + newFileName + "/config";
+
                 // create folder with name
                 Directory.CreateDirectory(newFolder);
                 Directory.CreateDirectory(newModsFolder);
                 Directory.CreateDirectory(newConfigFolder);
-                
+
                 // put file into mods folder inside the new folder
                 File.Move(path + file.Name, newModsFolder + "\\" + fileName);
-                
-                
+
+
                 // zip that folder
                 ZipFile.CreateFromDirectory(newFolder, newFolder + ".zip");
-                
+
             }
         }
     }
